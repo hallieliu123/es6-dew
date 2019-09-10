9e3fb964fb1511f83c3fe49df53b6ef074028ded
Ocean <hallieliu123@163.com>
2019-09-10 11:36:08 +0800

    iterator

---
+++ 08iterator.js
@@ -0,0 +1,56 @@
+const { log } = console;
+
+// 1.原生已部署Symbol.iterator属性的数据结构: array,arrayLike,string,set,map; 调用Symbol.iterator就可以返回遍历器对象
+    
+// 2.iterator接口实际应用
+    {
+        // 解构赋值
+        let s1 = new Set().add(1).add(2).add(3);
+        let [x,y,z] = s1;
+        log('x,y,z',x,y,z);
+        let [a,...b] = s1;
+        log('a,b',a,b);
+
+        //yeild* + 可遍历结构，会自动调用iterator接口
+        function* generator(){
+            yield 1;
+            yield* [2,3,4]; 
+            yield 5;
+        }
+        let g = generator();
+        log(g.next());
+        log(g.next());
+        log(g.next());
+        log(g.next());
+        log(g.next());
+    }
+// 3. 使用generator包装对象，用于使用for..of..遍历
+    {
+        function* iterateObj(obj){
+            for(let key of Object.keys(obj)){
+                yield [key,obj[key]]
+            }
+        }
+        let obj = {'name':'Miya','title':'princess','age':'15'};
+        for(let item of iterateObj(obj)){
+            log('[key,value]',item);
+        } 
+    }
+// 4. 模拟 iterator 接口
+    {
+        let arr = ['a','b','c'];
+        function it( arr ){
+            let index = -1;
+            return {
+                next:function(){
+                    index ++;
+                    return (index <= arr.length) ? { value:arr[index],done: false } : { value:undefined,done:true }
+                }
+            }
+        }
+        let i = it(arr);
+        log(i.next());
+        log(i.next());
+        log(i.next());
+        log(i.next());
+    }
